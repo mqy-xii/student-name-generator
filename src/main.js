35ef1db36d925ccc4df967b982baca3db0107ab5
May Mossor <benjamin.mossor@student.swic.edu>
2025-11-17 20:20:31 -0600

    Refactor generateStudentName function to be shorter

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ const button = document.querySelector("#button");
 const studentName = document.querySelector("#studentName");
 
 const generateStudentName = () => {
-  const id = getRandomInt(0, data.length - 1); // random int between 0 and array.length - 1
-  studentName.textContent = data[id].name;
+  studentName.textContent = data[getRandomInt(0, data.length - 1)].name;
 };
 
 button.addEventListener("click", generateStudentName);
