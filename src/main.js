e55e11108f2c3e7860165e7d24ab53c5566035c2
May Mossor <benjamin.mossor@student.swic.edu>
2025-11-17 20:19:48 -0600

    Create random name generator function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+import data from "./data.js";
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+const button = document.querySelector("#button");
+const studentName = document.querySelector("#studentName");
+
+const generateStudentName = () => {
+  const id = getRandomInt(0, data.length - 1); // random int between 0 and array.length - 1
+  studentName.textContent = data[id].name;
+};
+
+button.addEventListener("click", generateStudentName);
+
+// from MDN
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
