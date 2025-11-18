9d7e74a098ae05e8823bb83dd6e38c72c7f4d36f
May Mossor <benjamin.mossor@student.swic.edu>
2025-11-17 19:58:34 -0600

    Create mock student data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "May Mossor",
+  },
+  {
+    id: 2,
+    name: "Shrimp Scampi",
+  },
+  {
+    id: 3,
+    name: "Braden Reeves",
+  },
+  {
+    id: 4,
+    name: "Allie Zokal",
+  },
+  {
+    id: 5,
+    name: "Casey Stahl",
+  },
+];
